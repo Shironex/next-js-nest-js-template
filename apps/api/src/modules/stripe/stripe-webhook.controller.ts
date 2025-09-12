b09d225c54ccaec0e369abb7531000ce43fee8e5
Kacper <kacperlachowiczwp.pl@wp.pl>
2025-09-13 00:08:54 +0200

    feat(api): update stripe api to the latest version

--- apps/api/src/modules/stripe/stripe-webhook.controller.ts
+++ apps/api/src/modules/stripe/stripe-webhook.controller.ts
@@ -27,7 +27,7 @@ export class StripeWebhookController {
     this.stripe = new Stripe(
       this.configService.get('STRIPE_SECRET_KEY') || '',
       {
-        apiVersion: '2025-07-30.basil',
+        apiVersion: '2025-08-27.basil',
       },
     );
   }
--- apps/api/src/modules/stripe/stripe.service.ts
+++ apps/api/src/modules/stripe/stripe.service.ts
@@ -26,7 +26,7 @@ export class StripeService {
     this.stripe = new Stripe(
       this.configService.get('STRIPE_SECRET_KEY') || '',
       {
-        apiVersion: '2025-07-30.basil',
+        apiVersion: '2025-08-27.basil',
       },
     );
   }
